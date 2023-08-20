09c0ddd776eabb40478b85834ec6ee1b9d8a2e2a
Daniel Masek <daniel@masci.cz>
2023-08-20 22:53:27 +0200

    Slide panes with transitions

--- demo-slide/src/main/java/cz/masci/javafx/demo/SlideController.java
+++ demo-slide/src/main/java/cz/masci/javafx/demo/SlideController.java
@@ -1,22 +1,15 @@
 package cz.masci.javafx.demo;
 
-import javafx.animation.TranslateTransition;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.input.SwipeEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.util.Duration;
 
 public class SlideController {
 
   private static final double TRANSITION_DURATION = 0.5;
-
-  private Node currentNode;
 
   @FXML
   private AnchorPane centerPane;
@@ -31,98 +24,31 @@ public class SlideController {
   @FXML
   private GridPane grid2;
 
+  private SlideService slideService;
+
   public void initialize() {
     System.out.println("Initialize");
 
-    mainPane.widthProperty().addListener(this::onMainPaneWidthChange);
-    centerPane.setOnSwipeLeft(this::onSwipeNext);
-    centerPane.setOnSwipeRight(this::onSwipePrevious);
-//    grid1.onSwipeLeftProperty().addListener(this::onSwipeNext);
-//    grid1.onSwipeRightProperty().addListener(this::onSwipePrevious);
-//    grid2.onSwipeLeftProperty().addListener(this::onSwipeNext);
-//    grid2.onSwipeRightProperty().addListener(this::onSwipePrevious);
-
-    previousBtn.disableProperty().set(true);
-    nextBtn.disableProperty().set(false);
+    centerPane.setOnSwipeLeft(this::onNext);
+    centerPane.setOnSwipeRight(this::onPrevious);
 
     grid1.setVisible(true);
-    grid2.setVisible(true);
+    grid2.setVisible(false);
 
-    currentNode = grid1;
+    slideService = new SlideService(mainPane.widthProperty());
+    slideService.getNodeList().add(grid1);
+    slideService.getNodeList().add(grid2);
   }
 
   @FXML
-  private void onNext(ActionEvent event) {
+  private void onNext(Event event) {
     System.out.println("onNext");
-    swipeNext();
-  }
-
-  private void onSwipeNext(SwipeEvent event) {
-    System.out.println("onSwipeNext");
-    swipeNext();
+    slideService.next();
   }
 
   @FXML
-  private void onPrevious(ActionEvent event) {
+  private void onPrevious(Event event) {
     System.out.println("onPrevious");
-    swipePrevious();
-  }
-
-  private void onSwipePrevious(SwipeEvent event) {
-    System.out.println("onSwipePrevious");
-    swipePrevious();
-  }
-
-  private void swipePrevious() {
-    System.out.println("swipePrevious");
-
-    if (currentNode != grid2) {
-      return;
-    }
-
-    TranslateTransition translateTransitionPrevious = new TranslateTransition(Duration.seconds(TRANSITION_DURATION), grid1);
-    translateTransitionPrevious.setByX(mainPane.getWidth());
-    translateTransitionPrevious.play();
-
-    TranslateTransition translateTransitionCurrent = new TranslateTransition(Duration.seconds(TRANSITION_DURATION), grid2);
-    translateTransitionCurrent.setByX(mainPane.getWidth());
-    translateTransitionCurrent.play();
-
-    previousBtn.disableProperty().set(true);
-    nextBtn.disableProperty().set(false);
-
-    currentNode = grid1;
-  }
-
-  private void swipeNext() {
-    System.out.println("swipeNext");
-
-    if (currentNode != grid1) {
-      return;
-    }
-
-    TranslateTransition translateTransitionNext = new TranslateTransition(Duration.seconds(TRANSITION_DURATION), grid2);
-    translateTransitionNext.setByX(-mainPane.getWidth());
-    translateTransitionNext.play();
-
-    TranslateTransition translateTransitionCurrent = new TranslateTransition(Duration.seconds(TRANSITION_DURATION), grid1);
-    translateTransitionCurrent.setByX(-mainPane.getWidth());
-    translateTransitionCurrent.play();
-
-    //    grid1.setVisible(false);
-    previousBtn.disableProperty().set(false);
-    nextBtn.disableProperty().set(true);
-
-    currentNode = grid2;
-  }
-
-  private void onMainPaneWidthChange(ObservableValue<? extends Number> obsarvable, Number oldValue, Number newValue) {
-    System.out.println("Border main pane width changed to " + newValue.toString());
-    if (currentNode != grid1) {
-      grid1.translateXProperty().set(-newValue.doubleValue());
-    }
-    if (currentNode != grid2) {
-      grid2.translateXProperty().set(newValue.doubleValue());
-    }
+    slideService.previous();
   }
 }
